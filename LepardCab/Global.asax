ab2665dd7791f01bc987cef77d5f5b9785e2714e
M.EJAZ UR RAHMAN <ejazsidhu@gmail.com>
2017-11-24 09:23:39 +0500

    first commit

---
+++ LepardCab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LepardCab.MvcApplication" Language="C#" %>
---
+++ LepardCab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LepardCab.Startup))]
+namespace LepardCab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
