b46267a7af699d02c2d3aec0d0639b718cf85350
M.EJAZ UR RAHMAN <ejazsidhu@gmail.com>
2017-12-06 03:15:00 +0500

    sign in signup pages added for driver and customer

---
+++ LepardCab/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LepardCab.Models.DTOs
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "Contact is required")]
+        public string Contact { get; set; }
+        [Required(ErrorMessage ="Password is required")]
+        public string Password { get; set; }
+    }
+}
